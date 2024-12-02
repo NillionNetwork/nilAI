41b4784fc864e11876ad9233e49c30917ab567c2
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2024-12-02 10:07:57 +0000

    feat: first version of sev attestation

--- sev/main.go
+++ sev/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"C"
 	"fmt"
+	"unsafe"
 
 	"github.com/google/go-sev-guest/client"
 	"github.com/google/go-sev-guest/verify"
@@ -10,29 +12,61 @@ import (
 var device client.Device
 var quoteProvider client.QuoteProvider
 
-func main() {
+//export OpenDevice
+func OpenDevice() int {
 	var err error
-
 	if device, err = client.OpenDevice(); err != nil {
-		panic(fmt.Sprintf("failed to open device: %v", err))
+		fmt.Printf("failed to open device: %v\n", err)
+		return -1
 	}
+	return 0
+}
 
+//export GetQuoteProvider
+func GetQuoteProvider() int {
+	var err error
 	if quoteProvider, err = client.GetQuoteProvider(); err != nil {
-		panic(fmt.Sprintf("failed to get quote provider: %v", err))
+		fmt.Printf("failed to get quote provider: %v\n", err)
+		return -1
+	}
+	return 0
+}
+
+//export Init
+func Init() int {
+	if OpenDevice() != 0 || GetQuoteProvider() != 0 {
+		return -1
+	}
+	return 0
+}
+
+//export GetQuote
+func GetQuote(reportData *C.char) *C.char {
+	// Transform the reportData from C.char to []byte
+	reportDataBytes := [64]byte{0}
+	for i := 0; i < 64; i++ {
+		reportDataBytes[i] = byte(*(*C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(reportData)) + uintptr(i))))
 	}
 
 	// Use the device to get a quote.
-	reportData := [64]byte{0}
-	quote, err := client.GetQuoteProto(quoteProvider, reportData)
+	//quote, err := quoteProvider.GetRawQuote(*reportDataBytes)
+	quote, err := client.GetQuoteProto(quoteProvider, reportDataBytes)
 	if err != nil {
-		panic(fmt.Sprintf("failed to get raw quote: %v", err))
+		return C.CString(err.Error())
 	}
-
-	quote.String()
-
-	// Verify the quote.
-	err = verify.SnpAttestation(quote, verify.DefaultOptions())
-	if err != nil {
-		panic(fmt.Sprintf("failed to verify quote: %v", err))
-	}
+	return C.CString(quote.String())
 }
+
+//export VerifyQuote
+func VerifyQuote(quoteStr *C.char) int {
+	// Change the quoteStr from C.char to string
+	quote := []byte(C.GoString(quoteStr))
+	err := verify.SnpAttestation(&quote, verify.DefaultOptions())
+	if err != nil {
+		fmt.Printf("failed to verify quote: %v\n", err)
+		return -1
+	}
+	return 0
+}
+
+func main() {}
