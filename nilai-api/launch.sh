ccd40e873c8e290b1099362915f72e8efc1d0506
Matias Fontanini <matias.fontanini@gmail.com>
2025-02-07 14:57:27 -0800

    feat: expose http metrics via prometheus

--- nilai-api/launch.sh
+++ nilai-api/launch.sh
@@ -3,4 +3,10 @@
 set -euo pipefail
 
 uv run alembic upgrade head
-uv run gunicorn -c gunicorn.conf.py nilai_api.__main__:app
+
+# Setup the multiproc directory for all guvicon workers to expose metrics together
+export PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus-metrics
+rm -rf "$PROMETHEUS_MULTIPROC_DIR"
+mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
+
+exec uv run gunicorn -c gunicorn.conf.py nilai_api.__main__:app
