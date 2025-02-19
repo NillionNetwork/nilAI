92bd2d5e636bc79301ba0f58c630602f8587ac10
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2025-02-19 16:40:10 +0100

    feat: added nillion2025 token

--- nilai-api/src/nilai_api/auth/strategies.py
+++ nilai-api/src/nilai_api/auth/strategies.py
@@ -1,5 +1,6 @@
 from nilai_api.db.users import UserManager, UserModel
-from nilai_api.auth.jwt import validate_jwt
+from nilai_api.auth.jwt import JWTAuthResult, validate_jwt
+from nilai_api.config import ENVIRONMENT
 
 
 async def api_key_strategy(api_key):
@@ -7,7 +8,11 @@ async def api_key_strategy(api_key):
 
 
 async def jwt_strategy(jwt_creds):
-    result = validate_jwt(jwt_creds)
+    result = None
+    if ENVIRONMENT == "testnet" and jwt_creds == "Nillion2025":
+        result = JWTAuthResult(pub_key="Nillion2025", user_address="Nillion2025")
+    else:
+        result = validate_jwt(jwt_creds)
     user = await UserManager.check_api_key(result.user_address)
     if user:
         return user
