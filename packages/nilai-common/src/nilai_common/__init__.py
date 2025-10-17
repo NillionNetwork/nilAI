38c63faea3ef673baf7e14a18fb2e1e64d17ef54
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2025-10-17 13:10:59 +0200

    feat: improved nilai-common config

--- packages/nilai-common/src/nilai_common/__init__.py
+++ packages/nilai-common/src/nilai_common/__init__.py
@@ -33,7 +33,7 @@ from nilai_common.api_models import (
     EasyInputMessageParam,
     ResponseFunctionToolCallParam,
 )
-from nilai_common.config import SETTINGS, MODEL_SETTINGS
+from nilai_common.config import SETTINGS, MODEL_SETTINGS, MODEL_CAPABILITIES
 from nilai_common.discovery import ModelServiceDiscovery
 
 __all__ = [
@@ -55,6 +55,7 @@ __all__ = [
     "NVAttestationToken",
     "SETTINGS",
     "MODEL_SETTINGS",
+    "MODEL_CAPABILITIES",
     "SearchResult",
     "Source",
     "TopicResponse",
